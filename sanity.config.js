744351b976f7b87073034f534a22628a6f01edeb
PremAmbre0 <premambre80@gmail.com>
2024-05-04 11:50:47 +0530

    add: added media plugin

--- sanity.config.js
+++ sanity.config.js
@@ -10,7 +10,7 @@ import { structureTool } from 'sanity/structure'
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from './sanity/env'
 import { schema } from './sanity/schema'
-
+import {media} from 'sanity-plugin-media'
 export default defineConfig({
   basePath: '/studio',
   projectId,
@@ -18,7 +18,7 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-
+    media(),
     structureTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
